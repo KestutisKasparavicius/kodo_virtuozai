e34b1b9e560c263a669f6bd8120b5ca4fa3a4897
Kestutis <kestutis.kasparavicius@stud.ktmc.lt>
2025-02-13 11:46:17 +0200

    add api fetching

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,12 +2,16 @@ import React from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import css from '../styles/homeCSS.module.css'
+import City from '../components/City'
 const Home = () => {
   return (
     <div>
         <Header/>
             <main className={css.container}>
                 <h2>alio</h2>
+                <City city={'vilnius'} forecast={'long-term'}/>
+                <City city={'kaunas'} forecast={'long-term'}/>
+                <City city={'klaipeda'} forecast={'long-term'}/>
             </main>
         <Footer/>
     </div>
