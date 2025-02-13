2e0e4f364726a52b23d019c6e918f9e2347380e8
Kestutis <kestutis.kasparavicius@stud.ktmc.lt>
2025-02-13 09:54:44 +0200

    project scaffold

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header"
+function App() {
+
+  return (
+    <><Header/>
+    <div>
+      Alo
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import HeaderCSS from "../styles/headerCSS.module.css"
+const Header = () => {
+  return (
+    <header className={HeaderCSS.header}>
+        <nav className={HeaderCSS.nav}>
+            <ul className={HeaderCSS.list}>
+                <li><a href="#">Novel</a></li>
+                <li><a href="#">Romance</a></li>
+                <li><a href="#">Sci-fi</a></li>
+                <li><a href="#">Self Help</a></li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
+
+export default Header
